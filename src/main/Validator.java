f976e87fb96c9a6858df6357f13b3a4daa190e96
gustavonsberg <gustavonsberg@gmail.com>
2021-06-07 12:13:56 +0200

    Sohrthrth

--- src/main/Validator.java
+++ src/main/Validator.java
@@ -1,8 +1,12 @@
 package main;
 
+
 public class Validator {
     public boolean validate(PuzzleInfo target){
         //Validate puzzle
+        //JSONParser jsonParser = new JSONParser();
+
+
         return true;
     }
 }
