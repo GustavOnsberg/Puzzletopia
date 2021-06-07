25010bdc64982f22a0cfaf1ae82f0fbdb3f2cc85
gustavonsberg <gustavonsberg@gmail.com>
2021-06-07 23:23:35 +0200

    Sohrthrthdyhtfgdhdwdwdwdggtfgfgncfgdh

--- src/main/Piece.java
+++ src/main/Piece.java
@@ -9,4 +9,17 @@ public class Piece {
 
 
     ArrayList<FPoint> corners = new ArrayList<>();
+    boolean isSidePiece = false;
+    boolean isCornerPiece = false;
+
+
+    public void updateCornerArrayRotation(){
+        if (isSidePiece || isCornerPiece) {
+            ArrayList<Float> cornersDist = new ArrayList<>();
+            for (int i = 0; i < corners.size()-1; i++) {
+                cornersDist.add((float)Math.sqrt(Math.pow(corners.get(i).x - corners.get(i+1).x,2)+Math.pow(corners.get(i).y - corners.get(i+1).y,2)));
+            }
+            cornersDist.add((float)Math.sqrt(Math.pow(corners.get(corners.size()-1).x - corners.get(0).x,2)+Math.pow(corners.get(corners.size()-1).y - corners.get(0).y,2)));
+        }
+    }
 }
--- src/main/Puzzle.java
+++ src/main/Puzzle.java
@@ -15,17 +15,32 @@ public class Puzzle {
         return pieces;
     }
 
+    int highCornerCountCount = 0;
+    int midCornerCountCount = 0;
+    int lowCornerCountCount = 0;
+
+    int highCornerCount = -1;
+    int midCornerCount = -1;
+    int lowCornerCount = -1;
+
     ArrayList<Piece> pieces = new ArrayList<>();
 
     public static void main(String [] args) throws IOException, ParseException {
         Puzzle puzzle = new Puzzle();
-        puzzle.loadPuzzle("C:/Users/gusta/Desktop/Puzzle-3r-3c-3756-sol.json");
+        puzzle.loadPuzzle("C:/Users/gusta/Desktop/Puzzles_set_1/Puzzle-8r-10c-4267-rot.json");
     }
 
 
 
     public void loadPuzzle(String filePath) throws IOException, ParseException {
         pieces.clear();
+        highCornerCountCount = 0;
+        midCornerCountCount = 0;
+        lowCornerCountCount = 0;
+        highCornerCount = -1;
+        midCornerCount = -1;
+        lowCornerCount = -1;
+
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(filePath));
         JSONArray piecesJsonArray = (JSONArray) jsonObject.get("pieces");
@@ -65,7 +80,67 @@ public class Puzzle {
                 pieces.get(pieces.size()-1).corners.get(i).y += toCenterY;
             }
 
+            int cornerCount = pieces.get(pieces.size()-1).corners.size();
+
+            if (highCornerCount == -1){
+                highCornerCount = cornerCount;
+                highCornerCountCount++;
+            }
+            else if(highCornerCount == cornerCount){
+                highCornerCountCount++;
+            }
+            else if(lowCornerCount == -1){
+                if (cornerCount < highCornerCount){
+                    lowCornerCount = cornerCount;
+                    lowCornerCountCount++;
+                }
+                else{
+                    lowCornerCount = highCornerCount;
+                    lowCornerCountCount = highCornerCountCount;
+                    highCornerCount = cornerCount;
+                    highCornerCountCount = 1;
+                }
+            }
+            else if (lowCornerCount == cornerCount){
+                lowCornerCountCount++;
+            }
+            else if (midCornerCount == -1){
+                if (cornerCount < highCornerCount && cornerCount > lowCornerCount){
+                    midCornerCount = cornerCount;
+                    midCornerCountCount++;
+                }
+                else if(cornerCount > highCornerCount){
+                    midCornerCount = highCornerCount;
+                    midCornerCountCount = highCornerCountCount;
+                    highCornerCount = cornerCount;
+                    highCornerCountCount = 1;
+                }
+                else{
+                    midCornerCount = lowCornerCount;
+                    midCornerCountCount = lowCornerCountCount;
+                    lowCornerCount = cornerCount;
+                    lowCornerCountCount = 1;
+                }
+            }
+            else {
+                midCornerCountCount++;
+            }
         }
 
+
+        System.out.println("Number of corners - high: "+highCornerCount+"   mid: "+midCornerCount+"   low: "+lowCornerCount);
+        System.out.println("Number of pieces  - high: "+highCornerCountCount+"   mid: "+midCornerCountCount+"   low: "+lowCornerCountCount);
+
+        for (int i = 0; i < pieces.size(); i++) {
+            int cornerCount = pieces.get(i).corners.size();
+            if ((lowCornerCount == 4 && lowCornerCountCount == cornerCount) || lowCornerCount == -1){
+                pieces.get(i).isCornerPiece = true;
+                pieces.get(i).updateCornerArrayRotation();
+            }
+            else if(cornerCount == midCornerCount || midCornerCount == -1){
+                pieces.get(i).isSidePiece = true;
+                pieces.get(i).updateCornerArrayRotation();
+            }
+        }
     }
 }
