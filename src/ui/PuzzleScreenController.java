17265e9a38243db9e401c5490a6282a4476202dc
CookieNess <45033305+CookieNess@users.noreply.github.com>
2021-06-07 16:04:28 +0200

    Rotation

--- src/ui/PuzzleScreenController.java
+++ src/ui/PuzzleScreenController.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
@@ -19,25 +18,17 @@ import java.util.Random;
 
 public class PuzzleScreenController {
     public AnchorPane puzzleCanvas;
-    public double originalX, originalY;
+    public double originalPieceX, originalPieceY, originalMouseX, originalMouseY, mouseX, mouseY;
     public int puzzleBoardWidth = 1000;
     public int puzzleBoardHeight = 1000;
     public Image puzzlePicture = new Image("ui/test.png");
     private float puzzleScale = 100;
-    ArrayList<Point> pointCorners = new ArrayList<>();
     ArrayList<Color> testColors = new ArrayList<>();
     public PuzzleScreenController() {
     }
 
     @FXML
     public void initialize() {
-        for (int i = 0; i < MainMenuController.mainMenuController.getPuzzle().getPieces().size() - 1; i++) {
-            for (int j = 0; j < MainMenuController.mainMenuController.getPuzzle().getPieces().get(i).getCorners().size() - 1; j++) {
-                int pointX = (int) Math.floor(MainMenuController.mainMenuController.getPuzzle().getPieces().get(i).getCorners().get(j).getX() * puzzleScale);
-                int pointY = (int) Math.floor(MainMenuController.mainMenuController.getPuzzle().getPieces().get(i).getCorners().get(j).getY() * puzzleScale);
-                pointCorners.add(new Point(pointX, pointY));
-            }
-        }
 
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
@@ -46,7 +37,6 @@ public class PuzzleScreenController {
         int color = 1;
         for (int i = 0; i < MainMenuController.mainMenuController.getPuzzle().getPieces().size(); i++) {
             makePuzzlePiece(i, color);
-            i++;
             color++;
         }
     }
@@ -68,26 +58,54 @@ public class PuzzleScreenController {
 
         newPuzzlePiece.setCursor(Cursor.HAND);
         newPuzzlePiece.setOnMousePressed(event -> {
-            originalX = event.getSceneX();
-            originalY = event.getSceneY();
-
+            if (event.isPrimaryButtonDown()) {
+                originalPieceX = event.getSceneX();
+                originalPieceY = event.getSceneY();
+            } else if (event.isSecondaryButtonDown()) {
+                originalMouseX = MouseInfo.getPointerInfo().getLocation().x;
+                originalMouseY = MouseInfo.getPointerInfo().getLocation().y;
+            }
         });
+        Rotate rotate = new Rotate();
 
         newPuzzlePiece.setOnMouseDragged(event -> {
-            double offsetX = event.getSceneX() - originalX;
-            double offsetY = event.getSceneY() - originalY;
-            Rectangle piece = (Rectangle) (event.getSource());
-            piece.setX(piece.getX() + offsetX);
-            piece.setY(piece.getY() + offsetY);
-            originalX = event.getSceneX();
-            originalY = event.getSceneY();
-            puzzleShape.setTranslateX(piece.getX() + offsetX);
-            puzzleShape.setTranslateY(piece.getY() + offsetY);
+            if (event.isPrimaryButtonDown()) {
+                double offsetX = event.getSceneX() - originalPieceX;
+                double offsetY = event.getSceneY() - originalPieceY;
+                Rectangle piece = (Rectangle) (event.getSource());
+                piece.setX(piece.getX() + offsetX);
+                piece.setY(piece.getY() + offsetY);
+                originalPieceX = event.getSceneX();
+                originalPieceY = event.getSceneY();
+                puzzleShape.setTranslateX(piece.getX() + offsetX);
+                puzzleShape.setTranslateY(piece.getY() + offsetY);
 
-             // Debug piece position
-             // System.out.println("Piece X: " + piece.getX());
-             // System.out.println("Piece Y: " + piece.getY());
+                // Debug piece position
+                // System.out.println("Piece X: " + piece.getX());
+                // System.out.println("Piece Y: " + piece.getY());
+
+            } else if (event.isSecondaryButtonDown()) {
+                if (((Math.pow(MouseInfo.getPointerInfo().getLocation().x - originalMouseX,2) + Math.pow((MouseInfo.getPointerInfo().getLocation().y - originalMouseY),2)) > 2500)) {
+                    double a = Math.sqrt(Math.pow(MouseInfo.getPointerInfo().getLocation().x - originalMouseX,2) + Math.pow((MouseInfo.getPointerInfo().getLocation().y - originalMouseY),2));
+                    double b = Math.sqrt(Math.pow(mouseX - originalMouseX,2) + Math.pow(mouseY - originalMouseY, 2));
+                    double c = Math.sqrt(Math.pow(MouseInfo.getPointerInfo().getLocation().x - mouseX,2) + Math.pow(MouseInfo.getPointerInfo().getLocation().y - mouseY, 2));
+                    double ramma = Math.acos((Math.pow(a,2) + Math.pow(b,2) - Math.pow(c,2))/(2 * a * b));
+
+                    rotate.setPivotX(originalMouseX);
+                    rotate.setPivotY(originalMouseY);
+                    rotate.setAngle(ramma);
+                    System.out.println(a);
+                    System.out.println(b);
+                    System.out.println(c);
+                    System.out.println(ramma);
+                    newPuzzlePiece.getTransforms().addAll(rotate);
+
+                }
+                mouseX = MouseInfo.getPointerInfo().getLocation().x;
+                mouseY = MouseInfo.getPointerInfo().getLocation().y;
+            }
         });
+
 
         newPuzzlePiece.setStroke(Color.BLACK);
         puzzleCanvas.getChildren().add(new Group(newPuzzlePiece));
