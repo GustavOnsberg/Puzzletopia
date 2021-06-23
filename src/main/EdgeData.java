168239c025f4d6e01f5b90b296fa13fcc0b6dea8
gustavonsberg <gustavonsberg@gmail.com>
2021-06-23 18:44:37 +0200

    Sohrthrthdyhtfgdhdwdwdwdggtfgfgncfgdhghhfghghgyyywdwdefefefffedrgefefdrtfgdrtfgrffegrgwdwfhfhefthhthef

--- src/main/EdgeData.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.util.ArrayList;
-
-public class EdgeData {
-    ArrayList<Float> lengths = new ArrayList<>();
-    ArrayList<Float> angles = new ArrayList<>();
-    float cornerAngle = 0;
-}
